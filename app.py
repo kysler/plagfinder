11375e1690d469d335c9bac489591b62519bfd09
kysler <35858006+kysler@users.noreply.github.com>
2018-09-03 15:11:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, DataRequired
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask import Flask 
