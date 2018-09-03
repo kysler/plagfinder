ec801f0ff1b082a3bc29cedbf38ade184bb7856a
kysler <35858006+kysler@users.noreply.github.com>
2018-09-03 11:42:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,7 @@ class File(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
-
-	# User authentication information. The collation='NOCASE' is required
-	# to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+    username = db.Column(db.String(255, collation='NOCASE'), nullable=False, unique=True)
     email = db.Column(db.String(255, collation='NOCASE'), nullable=False, unique=True)
     email_confirmed_at = db.Column(db.DateTime())
     password = db.Column(db.String(255), nullable=False, server_default='')
