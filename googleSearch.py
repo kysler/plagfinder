fc2d1ef702099cb0451b8cb258c45a89e0ae15bd
kysler <35858006+kysler@users.noreply.github.com>
2018-09-24 11:02:38 +0800

    Update googleSearch.py

--- googleSearch.py
+++ googleSearch.py
@@ -28,6 +28,6 @@ def googleSearch(uploaded_file):
         url = 'https://google.com/search?q=' + item
         response = requests.get ( url )
         soup = BeautifulSoup(response.text, 'html.parser')
-        for item in soup.find('cite').text:
-            links.append(item)
+        for item in link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+            print re.split(":(?=http)",link["href"].replace("/url?q=",""))
     return links[0:5]
