5e49d9b4bd13c8f666dd9057e23549db3dca70ad
kysler <35858006+kysler@users.noreply.github.com>
2018-09-25 01:16:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def startpage ( ):
 @app.route ('/index', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return render_template ( 'url_for('user.login')' )
+        return redirect ( 'url_for('user.login')' )
 
 @app.route ( '/upload', methods=[ 'POST', 'GET' ] )
 @login_required
