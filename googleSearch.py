47376e388dc5f492846301ac9971d3bb1f54b008
Copy!Not <35858006+kysler@users.noreply.github.com>
2018-10-22 22:40:56 +0800

    Update googleSearch.py

--- googleSearch.py
+++ googleSearch.py
@@ -10,7 +10,7 @@ import ctypes
 import docx
 import re
 import webbrowser
-from googlesearch import search
+import time
 from itertools import chain
 
 def getText(filename):
@@ -30,14 +30,20 @@ def googleSearch(uploaded_file):
     results = list(OrderedDict.fromkeys(links))[0:5]
     return '[-]'.join(results)
 
-
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 def searchText(uploaded_file):
     text = uploaded_file
     sentences = tokenize.sent_tokenize(text)
     links = []
     for item in sentences:
-        for url in search(item, stop=20, user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'):
-            links.append(url)
+        keyword = urllib.parse.quote_plus(item)
+        google_url = 'https://www.google.com/search?q={}&num={}'.format(keyword, 1)
+        r = requests.get(google_url, headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        url = soup.select('.r a')
+        urlclean = url[0]['href']
+        links.append(urlclean)
+        time.sleep(3)
     results = list(OrderedDict.fromkeys(links))[0:5]
     return '[-]'.join(results)
