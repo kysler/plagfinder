af1755cb86235a8e5c7a6ea15acb3b1f20461c12
Copy!Not <35858006+kysler@users.noreply.github.com>
2018-10-22 16:27:47 +0800

    Update googleSearch.py

--- googleSearch.py
+++ googleSearch.py
@@ -37,7 +37,7 @@ def searchText(uploaded_file):
     sentences = tokenize.sent_tokenize(text)
     links = []
     for item in sentences:
-        for url in search(item, stop=1):
+        for url in search(item,stop=1,user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'):
             links.append(url)
     results = list(OrderedDict.fromkeys(links))[0:5]
     return '[-]'.join(results)
