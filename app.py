55fc5ed163a9445029e1fe3784c48bc7745d4f48
Copy!Not <35858006+kysler@users.noreply.github.com>
2018-10-22 12:27:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -212,6 +212,7 @@ def create_app(object):
     return app
 
 if __name__ == "__main__":
+    app = create_app('app')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
 #Lol
