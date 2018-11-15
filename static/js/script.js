77ae797fadedb7964ba1e8ef2903bba4e195a7f7
aressanramon <43092929+aressanramon@users.noreply.github.com>
2018-11-16 04:18:55 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".my-progress-bar").circularProgress({
                 line_width: 18,
-                color: '#FF0000',
+                color: '#1E90FF',
                 starting_position: 75,
                 percent: 0,
                 percentage: true,
