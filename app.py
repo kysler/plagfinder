0af36141f9e7cb3778036ce5b70c4bdae8ae94f3
Copy!Not <35858006+kysler@users.noreply.github.com>
2018-11-20 05:08:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -138,9 +138,7 @@ def create_app(config_class=configClass):
     @app.route ( '/homepage', methods=[ 'POST', 'GET' ] )
     @login_required
     def upload():
-        current = []
-        current.append(current_user.roles)
-        return render_template ('base.html', current=current)
+        return render_template ('base.html', current=current_user.roles)
 
     @app.route('/admin')
     def adminpage():
